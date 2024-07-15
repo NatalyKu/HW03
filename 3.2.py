736c2c88a3021b9409b150c56f05f7f83d6723c2
NatalyKu <nataly.kuksa.uk@gmail.com>
2024-07-15 09:56:42 +0200

    changed if statment

--- 3.2.py
+++ 3.2.py
@@ -2,14 +2,15 @@ import random
 
 def get_numbers_ticket(min_numb, max_numb, quantity):
     list_numbers = set()
-    if min_numb <1 or max_numb > 1000:
+    if min_numb <1 or max_numb > 1000 and max_numb>min_numb and len(list_numbers)>quantity:
         return print(f"You numbers are wrong")
-    
+        
     else:
         while quantity != len(list_numbers):
             list_numbers.add(random.randint(min_numb, max_numb))
             numbers = sorted(list_numbers)
-        return numbers              
+        return numbers
+   
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)        
+lottery_numbers = get_numbers_ticket(10,4,2)        
 print("Ваші лотерейні числа:", lottery_numbers)
